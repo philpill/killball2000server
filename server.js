1c215998c6f5d9a186328f2a21a1acd39dcc8250
philpill <github@philpill.net>
2014-01-18 14:17:15 +0000

    config

--- server.js
+++ server.js
@@ -15,4 +15,4 @@ app.get('/', function(req, res){
 // ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 // port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-app.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
+app.listen((process.env.OPENSHIFT_NODEJS_PORT || 8080), process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
