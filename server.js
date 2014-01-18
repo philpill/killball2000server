633e20a310ab59e631559305a3b05023cdd9f850
philpill <github@philpill.net>
2014-01-18 20:13:11 +0000

    api

--- server.js
+++ server.js
@@ -12,6 +12,50 @@ app.get('/', function(req, res){
         res.render('static/index');
 });
 
+app.get('/api/game/teams', function(req, res){
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization');
+    res.json({
+        "teams": [{
+            "name": "Orcland Raiders",
+            "race": "Orc",
+            "players": [
+                { "number": 1, "name": "OR-a", "y": 1, "x": 10, "position": "black orc" },
+                { "number": 2, "name": "OR-b", "y": 3, "x": 11, "position": "black orc" },
+                { "number": 3, "name": "OR-c", "y": 4, "x": 12, "position": "blitzer" },
+                { "number": 4, "name": "OR-d", "y": 5, "x": 11, "position": "thrower" },
+                { "number": 5, "name": "OR-e", "y": 6, "x": 12, "position": "lineman" },
+                { "number": 6, "name": "OR-f", "y": 7, "x": 12, "position": "lineman" },
+                { "number": 7, "name": "OR-g", "y": 8, "x": 12, "position": "lineman" },
+                { "number": 8, "name": "OR-h", "y": 9, "x": 11, "position": "lineman" },
+                { "number": 9, "name": "OR-i", "y": 10, "x": 12, "position": "lineman" },
+                { "number": 10, "name": "OR-j", "y": 11, "x": 11, "position": "lineman" },
+                { "number": 11, "name": "OR-k", "y": 13, "x": 10, "position": "goblin" }
+            ],
+            "colour": "red"
+        },
+        {
+            "name": "Reikland Reavers",
+            "race": "Human",
+            "players": [
+                { "number": 1, "name": "RR-a", "y": 5, "x": 22, "position": "blitzer" },
+                { "number": 2, "name": "RR-b", "y": 3, "x": 17, "position": "blitzer" },
+                { "number": 3, "name": "RR-c", "y": 4, "x": 14, "position": "thrower" },
+                { "number": 4, "name": "RR-d", "y": 5, "x": 13, "position": "catcher" },
+                { "number": 5, "name": "RR-e", "y": 6, "x": 13, "position": "catcher" },
+                { "number": 6, "name": "RR-f", "y": 7, "x": 13, "position": "lineman" },
+                { "number": 7, "name": "RR-g", "y": 8, "x": 13, "position": "lineman" },
+                { "number": 8, "name": "RR-h", "y": 9, "x": 13, "position": "lineman" },
+                { "number": 9, "name": "RR-i", "y": 10, "x": 14, "position": "lineman" },
+                { "number": 10, "name": "RR-j", "y": 11, "x": 17, "position": "lineman" },
+                { "number": 11, "name": "RR-k", "y": 9, "x": 19, "position": "lineman" }
+            ],
+            "colour": "blue"
+        }]
+    });
+});
+
 // ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 // port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
