fe323e899a8c7d860fa948c0149fa2f68bd21755
philpill <github@philpill.net>
2014-01-20 00:08:04 +0000

    mongoose config

--- config.openshift.js
+++ config.openshift.js
@@ -1,14 +1,11 @@
-// default to a 'localhost' configuration:
+module.exports = {
 
-var connection_string = '127.0.0.1:27017/killball2000';
+    database : {
 
-// if OPENSHIFT env variables are present, use the available connection info:
-
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-
-    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-    process.env.OPENSHIFT_APP_NAME;
+        url: process.env.OPENSHIFT_MONGODB_DB_HOST,
+        port: process.env.OPENSHIFT_MONGODB_DB_PORT,
+        user: process.env.OPENSHIFT_MONGODB_DB_USERNAME,
+        pass: process.env.OPENSHIFT_MONGODB_DB_PASSWORD,
+        db: process.env.OPENSHIFT_APP_NAME
+    }
 }
